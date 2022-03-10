df75efc1392d397515b4271b9845a5a263666e23
Thomas-Wei-666 <72587206+Thomas-Wei-666@users.noreply.github.com>
2022-03-10 23:29:28 +0800

    finish hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,150 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[][] grid;
+    private int N;
+    private WeightedQuickUnionUF weightedQuickUnionUF;
+    private int countOpen;
+    private boolean firstOpenFlag = true;
+
+
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        this.grid = new boolean[N][N];
+        this.N = N;
+        this.weightedQuickUnionUF = new WeightedQuickUnionUF(N * N + 1);
+        this.countOpen = 0;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        isValid(row);
+        isValid(col);
+        if (isOpen(row, col)) {
+            return weightedQuickUnionUF.connected(linerConvert(row, col), N * N);
+        }
+        return false;
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        isValid(row);
+        isValid(col);
+        if (isOpen(row, col)) {
+            return;
+        }
+        if (firstOpenFlag) {
+            weightedQuickUnionUF.union(linerConvert(row, col), N * N);
+            firstOpenFlag = false;
+        } else {
+            grid[row][col] = true;
+            modifyFull(row, col);
+            countOpen++;
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        isValid(row);
+        isValid(col);
+        return grid[row][col];
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return countOpen;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        int TopLoc = findFullInRowX(0);
+        int BottomLoc = findFullInRowX(N - 1);
+        if (TopLoc < 0 || BottomLoc < 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void isValid(int lenth) {
+        if (lenth >= N || lenth < 0) {
+            throw new IllegalArgumentException("row or col must >= 0 or < N");
+        }
+    }
+
+    private int linerConvert(int row, int col) {
+        return row * N + col;
+    }
+
+    private int findFullInRowX(int rowX) {
+        isValid(rowX);
+        int res = -1;
+        int i = 0;
+        for (; i < N; i++) {
+            if (isFull(rowX, i)) {
+                res = i;
+                break;
+            }
+        }
+        if (res == -1) {
+            return -1;
+        } else {
+            return linerConvert(rowX, res);
+        }
+    }
+
+    private int surroundCheck(int row, int col) {
+        if (row - 1 >= 0) {
+            if (isFull(row - 1, col)) {
+                return linerConvert(row - 1, col);
+            }
+        }
+        if (col - 1 >= 0) {
+            if (isFull(row, col - 1)) {
+                return linerConvert(row, col - 1);
+            }
+        }
+        if (row + 1 < N) {
+            if (isFull(row + 1, col)) {
+                return linerConvert(row + 1, col);
+            }
+        }
+        if (col + 1 < N) {
+            if (isFull(row, col + 1)) {
+                return linerConvert(row, col + 1);
+            }
+        }
+        return -1;
+    }
+
+    private void modifyFull(int row, int col) {
+        if (surroundCheck(row, col) == -1) {
+            return;
+        } else {
+            weightedQuickUnionUF.union(linerConvert(row, col), N * N);
+            if (row - 1 >= 0) {
+                if (isOpen(row - 1, col) && !isFull(row - 1, col)) {
+                    modifyFull(row - 1, col);
+                }
+            }
+            if (row + 1 < N) {
+                if (isOpen(row + 1, col) && !isFull(row + 1, col)) {
+                    modifyFull(row + 1, col);
+                }
+            }
+            if (col - 1 >= 0) {
+                if (isOpen(row, col - 1) && !isFull(row, col - 1)) {
+                    modifyFull(row, col - 1);
+                }
+            }
+            if (col + 1 < N) {
+                if (isOpen(row, col + 1) && !isFull(row, col + 1)) {
+                    modifyFull(row, col + 1);
+                }
+            }
+        }
+
+    }
+
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,58 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
+    private Percolation percolation;
+    private int T;
+    private double[] x;
+    private int N;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N or T must be positive");
+        }
+        percolation = pf.make(N);
+        this.T = T;
+        this.N = N;
+        x = new double[T];
+
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j < N * N; j++) {
+                if (percolation.percolates()) {
+                    break;
+                }
+                percolation.open(StdRandom.uniform(0, N - 1), StdRandom.uniform(0, N - 1));
+            }
+            x[i] = percolation.numberOfOpenSites() / (N * N * 1.0f);
+        }
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(x);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(x);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        double mean = mean();
+        double sigma = stddev();
+        return mean - 1.96 * sigma / Math.sqrt(T);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        double mean = mean();
+        double sigma = stddev();
+        return mean + 1.96 * sigma / Math.sqrt(T);
+    }
 
 }
