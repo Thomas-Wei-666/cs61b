90bd05929ae636ddf45151c1bba979fa71beb3f2
Thomas-Wei-666 <72587206+Thomas-Wei-666@users.noreply.github.com>
2022-03-24 16:52:48 +0800

    finish required task, leaving the optional ones to be done

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,7 +1,10 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
     /* Inherits public fields:
@@ -10,20 +13,58 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private Maze maze;
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+
+    private static final int INFINITY = 9999;
+
+
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
-    /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+    /**
+     * Conducts a breadth first search of the maze starting at the source.
+     */
+    private void bfs(int s) {
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 1; i < maze.V(); i++) {
+            distTo[i] = INFINITY;
+        }
+
+        marked[s] = true;
+        announce();
+        q.add(s);
+
+        while (!q.isEmpty()) {
+            int v = q.remove();
+            if (v == t){
+                return;
+            }
+            for (int w :
+                    maze.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] = true;
+                    q.add(w);
+                    announce();
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs(s);
     }
 }
 
